7ed0d524f575d6686614641d4532acbb390a2eb7
AndyVN2000 <andyvu@outlook.dk>
2025-01-05 20:44:20 +0100

    Initial commit for C# branch

---
+++ Player.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+
+	public Vector2 ScreenSize; // Size of the game window.
+}
